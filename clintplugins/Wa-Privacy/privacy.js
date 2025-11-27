84c74111a99c20ffd0e262cf1b5bb5c691ea59cd
Ivan Schroll <134553365+thebitnomad@users.noreply.github.com>
2025-11-27 11:37:22 -0300

    Refactor privacy.js for improved readability and localization

--- clintplugins/Wa-Privacy/privacy.js
+++ clintplugins/Wa-Privacy/privacy.js
@@ -1,40 +1,41 @@
 module.exports = async (context) => {
 
-const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
+    const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
     await ownerMiddleware(context, async () => {
 
-    const { client, m } = context;
+        const { client, m } = context;
 
-const Myself = await client.decodeJid(client.user.id);
-    
-    const {
-                readreceipts,
-                profile,
-                status,
-                online,
-                last,
-                groupadd,
-                calladd
+        const Myself = await client.decodeJid(client.user.id);
+        
+        const {
+            readreceipts,
+            profile,
+            status,
+            online,
+            last,
+            groupadd,
+            calladd
         } = await client.fetchPrivacySettings(true);
         
-        const fnn = `*Current Privacy Settings*
+        const fnn = `*Configurações de Privacidade Atuais*
 
-* Name : ${client.user.name}
+* Nome: ${client.user.name}
 * Online: ${online}
-* Profile picture : ${profile}
-* Last seen : ${last}
-* Read receipt : ${readreceipts}
-* Group add : ${groupadd}
-* Status : ${status}
-* Call add : ${calladd}`;
+* Foto de perfil: ${profile}
+* Visto por último: ${last}
+* Confirmação de leitura: ${readreceipts}
+* Quem pode adicionar em grupos: ${groupadd}
+* Status: ${status}
+* Quem pode te ligar: ${calladd}`;
 
+        const avatar = await client.profilePictureUrl(Myself, 'image').catch(_ => 'https://telegra.ph/file/b34645ca1e3a34f1b3978.jpg');
 
-const avatar = await client.profilePictureUrl(Myself, 'image').catch(_ => 'https://telegra.ph/file/b34645ca1e3a34f1b3978.jpg');
+        await client.sendMessage(
+            m.chat,
+            { image: { url: avatar }, caption: fnn },
+            { quoted: m }
+        );
 
-await client.sendMessage(m.chat, { image: { url: avatar}, caption: fnn}, { quoted: m}) 
-
-
-})
+    });
 
 }
-        
