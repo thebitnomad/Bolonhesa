6ce4c20411b7a4ad7c21d5fd28d36aeb76021b00
Ivan Schroll <134553365+thebitnomad@users.noreply.github.com>
2025-11-27 18:16:11 -0300

    Update fullpp.js

--- clintplugins/Owner/fullpp.js
+++ clintplugins/Owner/fullpp.js
@@ -5,39 +5,41 @@ module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
         const { client, m, text, Owner, generateProfilePicture, botNumber, mime, msgToxic } = context;
 
-try {
-const fs = require("fs");
+        try {
+            const fs = require("fs");
 
-if(!msgToxic) { m.reply('REPLY TO A FUCKING IMAGE RIGHT NOW!'); return } ;
+            if (!msgToxic) {
+                m.reply(
+                    "◈━━━━━━━━━━━━━━━━◈\n" +
+                    "│❒ Responda a uma imagem para usar este comando.\n" +
+                    "◈━━━━━━━━━━━━━━━━◈"
+                );
+                return;
+            }
 
+            let media;
+            if (msgToxic.imageMessage) {
+                media = msgToxic.imageMessage;
+            } else {
+                m.reply(
+                    "◈━━━━━━━━━━━━━━━━◈\n" +
+                    "│❒ Isto não é uma imagem.\n" +
+                    "│❒ Por favor, responda a uma imagem válida.\n" +
+                    "◈━━━━━━━━━━━━━━━━◈"
+                );
+                return;
+            }
 
-let media;
-if (msgToxic.imageMessage) {
-     media = msgToxic.imageMessage
+            const medis = await client.downloadAndSaveMediaMessage(media);
 
-  } else {
-    m.reply('THIS IS NOT A FUCKING IMAGE! REPLY TO AN IMAGE!'); return
-  } ;
+            const { img } = await generateProfilePicture(medis);
 
-var medis = await client.downloadAndSaveMediaMessage(media);
-
-
-
-                    var {
-                        img
-                    } = await generateProfilePicture(medis)
-
-
-
-
-
-
-client.query({
+            await client.query({
                 tag: 'iq',
                 attrs: {
                     target: undefined,
                     to: S_WHATSAPP_NET,
-                    type:'set',
+                    type: 'set',
                     xmlns: 'w:profile:picture'
                 },
                 content: [
@@ -47,20 +49,23 @@ client.query({
                         content: img
                     }
                 ]
-            })
+            });
 
-                    fs.unlinkSync(medis)
-                    m.reply("Bot Profile Picture Updated")
+            fs.unlinkSync(medis);
 
-} catch (error) {
+            m.reply(
+                "◈━━━━━━━━━━━━━━━━◈\n" +
+                "│❒ Foto de perfil do bot atualizada com sucesso.\n" +
+                "◈━━━━━━━━━━━━━━━━◈"
+            );
 
-m.reply("An error occured while updating bot profile photo\n" + error)
-
-}
-
-                })
-
-
-
-
-}
+        } catch (error) {
+            m.reply(
+                "◈━━━━━━━━━━━━━━━━◈\n" +
+                "│❒ Ocorreu um erro ao atualizar a foto de perfil do bot.\n" +
+                "│❒ Detalhes: " + error + "\n" +
+                "◈━━━━━━━━━━━━━━━━◈"
+            );
+        }
+    });
+};
